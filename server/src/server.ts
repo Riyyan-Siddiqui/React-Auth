d8718a6b796c852575b813eafff973aa209b8513
Riyyan Siddiqui <riyyan.s.24@gmail.com>
2026-04-17 12:02:53 +0500

    Resolving cors issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ if (NODE_ENV === "production") {
 
 app.use(
   cors({
-    origin: true,
+    origin: ["react-auth-gules-pi.vercel.app", "https://react-auth-git-main-riyyan-siddiquis-projects.vercel.app", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
